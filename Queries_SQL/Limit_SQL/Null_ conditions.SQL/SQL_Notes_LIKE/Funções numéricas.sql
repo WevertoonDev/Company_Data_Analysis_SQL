1c4b407496c275e2eae30765a441ea49a6bb9cc7
Weverton Martins <wevertonm22@icloud.com>
2026-04-21 18:44:48 -0300

    Funções numéricas

---
+++ Queries_SQL/Limit_SQL/Null_ conditions.SQL/SQL_Notes_LIKE/Funções numéricas.sql
@@ -0,0 +1,5 @@
+SELECT AVG(faturamento_bruto), ROUND (AVG(faturamento_bruto),2) FROM faturamento;
+
+SELECT CEIL(faturamento_bruto), CEIL(despesas) FROM faturamento;
+
+SELECT FLOOR(faturamento_bruto), FLOOR(despesas) FROM faturamento;
