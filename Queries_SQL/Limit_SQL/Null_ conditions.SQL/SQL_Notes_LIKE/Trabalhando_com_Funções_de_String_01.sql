7d708b6dcb12ca4ab6a97ff8d6458406c0b09e3f
Weverton Martins <wevertonm22@icloud.com>
2026-04-21 14:32:53 -0300

    Trabalhando com funções de string

---
+++ Queries_SQL/Limit_SQL/Null_ conditions.SQL/SQL_Notes_LIKE/Trabalhando_com_Funções_de_String_01.sql
@@ -0,0 +1,3 @@
+SELECT (' O colaboradoro(a) ' || nome || ' de CPF ' || cpf || ' possui o seguinte endereço: '
+        || endereco) as texto
+        from Colaboradores;
---
+++ Queries_SQL/Limit_SQL/Null_ conditions.SQL/SQL_Notes_LIKE/Trabalhando_com_Funções_de_String_02.sql
@@ -0,0 +1,7 @@
+SELECT nome,LENGTH(cpf) qtd 
+from Colaboradores
+WHERE qtd = 11;
+
+SELECT COUNT(*), LENGTH(cpf) qtd
+from Colaboradores
+where qtd = 11;
